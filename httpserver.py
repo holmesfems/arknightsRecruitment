65baabdb4f0ae153de5a6c912ff3bbfd87d67750
holmesfems <holmesfems@gmail.com>
2025-09-29 02:02:17 +0900

    add description

--- httpserver.py
+++ httpserver.py
@@ -4,7 +4,7 @@ import uvicorn
 from fastapi import FastAPI
 #from flask import Flask,request
 import json
-from pydantic import BaseModel
+from pydantic import BaseModel,Field
 
 port = int(os.environ["PORT"])
 print(f"Server port = {port}")
@@ -12,11 +12,11 @@ print(f"Server port = {port}")
 app = FastAPI()
 
 class TagData(BaseModel):
-    text: str
+    text: str = Field(description="The raw data of OCR result. Each tag should be separated by line breaks")
 
 class TagReplyData(BaseModel):
-    title: str
-    reply: str
+    title: str = Field(description="The recognized tags")
+    reply: str = Field(description="The result to be displayed on screen")
 
 @app.get("/")
 async def root():
